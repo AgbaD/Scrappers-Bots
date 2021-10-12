bf36ee3d492627d16a3b0fa1de65bdad2b05b2ec
Damilare Agba <damilareagba1@gmail.com>
2021-10-12 17:53:30 +0100

    api client complete

--- lyrically/crawler/client.py
+++ lyrically/crawler/client.py
@@ -34,6 +34,68 @@ def get_hits(query):
     return result["response"]["hits"]
 
 
+def search_song(query):
+    songs = get_hits(query)
+    result = []
+    for song in songs:
+        song_id = song['result']['id']
+        song_title = song['result']['full_title']
+        artist = song['result']['primary_artist']['name']
+        s = (song_id, song_title, artist)
+        result.append(s)
+    return result
+
+
+def get_song(id):
+    path = f"songs/{id}"
+    url = '/'.join([base_url, path])
+
+    params = {'text_format': 'plain'}
+    token = f'Bearer {access_token}'
+    headers = {
+        'Authorization': token,
+        'application': 'BLyrics',
+        'User-Agent': 'https://github.com/AgbaD/lyrically'
+    }
+    resp = requests.get(url, params=params, headers=headers)
+    if resp.status_code != 200:
+        return None
+    song = json.loads(resp.text)
+
+    base = song['response']['song']
+    description = base['description']['plain']
+    title = base['full_title']
+    recording_location = base['recording_location']
+    release_date = base['release_date_for_display']
+    lyrics_url = base['url']
+    artist = base['album']['artist']['name']
+    album_title = base['album']['name']
+    album_id = base['album']['id']
+
+    song = [description, title, recording_location, release_date, lyrics_url, artist, album_title, album_id]
+    return song
+
+
+def get_album(id):
+    path = f"albums/{id}"
+    url = '/'.join([base_url, path])
+
+    params = {'text_format': 'plain'}
+    token = f'Bearer {access_token}'
+    headers = {
+        'Authorization': token,
+        'application': 'BLyrics',
+        'User-Agent': 'https://github.com/AgbaD/lyrically'
+    }
+    resp = requests.get(url, params=params, headers=headers)
+    if resp.status_code != 200:
+        return None
+    album = json.loads(resp.text)
+    title = album['response']['album']['full_title']
+    release_date = album['response']['album']['release_date']
+    return [title, release_date]
+
+
 def get_artist_id(artist):
     result = get_hits(artist)
     ans = {}
@@ -68,8 +130,3 @@ def get_artist(id):
     return names, description, facebook_name, instagram_name, twitter_name
 
 
-if __name__ == "__main__":
-    idd = get_artist_id('drake')
-    print(idd)
-    for v in idd.values():
-        print(get_artist(v))
